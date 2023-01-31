8e01a6578a89e72c73561920efac97c5dc8bf46d
jovro <jorge020499@gmail.com>
2023-01-30 20:59:07 -0500

    Modified sql, added dist to deploy statically

--- database/db.sql
+++ database/db.sql
@@ -6,3 +6,9 @@ CREATE TABLE tasks(
   done BOOLEAN NOT NULL DEFAULT 0,
   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+INSERT INTO tasks(id,title,description) VALUES
+  (1,'First task','First description'),
+  (2,'Second task','Second description'),
+  (3,'Third task','Third description');
